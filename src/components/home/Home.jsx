57ea0dac04097dcfc327526f597ba9a9d1b50642
ngothanhthai297 <ngothai653@gmail.com>
2021-10-04 20:10:38 +0700

    Fix bug key unique

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -64,7 +64,7 @@ export function Home() {
 
   const genreList = genres.map((item, index) => {
     return (
-      <nav className="link-effect-1">
+      <nav className="link-effect-1" key={index}>
         <div className="Link-hover" key={index} onClick={() => {
           handleGenreClick(item.id);
         }}><span data-hover={item.name}> {item.name}</span></div>
@@ -80,7 +80,7 @@ export function Home() {
           <Link to={`/movie/${value.id}`}>
             <img className="img-fluids" src={value.poster} alt={value.title}></img>
           </Link>
-          <a class="info" href={`/movie/${value.id}`}>Xem</a>
+          <a className="info" href={`/movie/${value.id}`}>Xem</a>
         </div>
         <div className="title-movie">
           {value.title}
@@ -122,8 +122,8 @@ export function Home() {
           </Link>
         </div>
         <div className="mt-3">
-          <p style={{ fontWeight: "bolder" , textAlign: "center" }}>{item.title}</p>
-          <p style={{textAlign: "center" }}>Rated: {item.rating}</p>
+          <p style={{ fontWeight: "bolder", textAlign: "center" }}>{item.title}</p>
+          <p style={{ textAlign: "center" }}>Rated: {item.rating}</p>
           {/* <ReactStars
             count={item.rating}
             size={20}
@@ -134,197 +134,193 @@ export function Home() {
     );
   });
   // Lấy các tấm hình:
-  const images = movieByGenre.slice(0, 12).map((i) => {
+  const images = movieByGenre.slice(0, 12).map((i,index) => {
     return (
-      <img src={i.poster} alt={i.title} className="pic" />
+      <img src={i.poster} alt={i.title} className="pic" key={index} />
     );
   });
 
   return (
-
-    <div className="main-container">
-      <div className="header">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <nav>
-                <ul className="menu">
-                  <li className="nav-hover"><a href="/">Home</a></li>
-                  <li className="nav-hover"><a href="/trending/all/day">Trending</a></li>
-                  <li className="nav-hover"><a href="/discover/tv">TV</a></li>
-                  <li className="nav-hover"><a href="/people/popular">People</a></li>
-                  <li className="nav-hover"><a href="/search">Search</a></li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </div>
-
-        <div className="baner-header">
+      <div className="main-container">
+        <div className="header">
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-                <div className="text-effect">
-                  <h1 className="neon" data-text="THE MOVIE">THE MOVIE</h1>
-                  <div className="gradient"></div>
-                  <div className="spotlight"></div>
+                <nav>
+                  <ul className="menu">
+                    <li className="nav-hover"><a href="/">Home</a></li>
+                    <li className="nav-hover"><a href="/trending/all/day">Trending</a></li>
+                    <li className="nav-hover"><a href="/discover/tv">TV</a></li>
+                    <li className="nav-hover"><a href="/people/popular">People</a></li>
+                    <li className="nav-hover"><a href="/search">Search</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+
+          <div className="baner-header">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-effect">
+                    <h1 className="neon" data-text="THE MOVIE">THE MOVIE</h1>
+                    <div className="gradient"></div>
+                    <div className="spotlight"></div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-
-      <div className="baner-movie">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <RBCarousel
-                autoplay={true}
-                pauseOnVisibility={true}
-                slidesshowSpeed={5000}
-                version={4}
-                indicators={false}
-              >
-                {movies}
-              </RBCarousel>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* Thể loại phim */}
-      <div className="genre-list">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div id="content">
-                <section className="links">
-                  {genreList}
-                </section>
+        <div className="baner-movie">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <RBCarousel
+                  autoplay={true}
+                  pauseOnVisibility={true}
+                  slidesshowSpeed={5000}
+                  version={4}
+                  indicators={false}
+                >
+                  {movies}
+                </RBCarousel>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-
-      {/* Danh sách phim */}
-      <div className="list-movie">
-        <div className="container">
-          <div className="row">
-
-            <div className="col-md-12">
-              <div className="list-movie">
-                <div className="row mt-3">{movieList}</div>
+        {/* Thể loại phim */}
+        <div className="genre-list">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div id="content">
+                  <section className="links">
+                    {genreList}
+                  </section>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Xu hướng phim */}
 
-      <div className="person-list">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="person">
-                <p className="font-weight-bold-1  line-1" style={{ color: "white", paddingTop: 30 }}>
-                  <a href="#" className="hover-trending">NHỮNG DIỄN VIÊN</a> XU HƯỚNG TRONG TUẦN
+        {/* Danh sách phim */}
+        <div className="list-movie">
+          <div className="container">
+            <div className="row">
+
+              <div className="col-md-12">
+                <div className="list-movie">
+                  <div className="row mt-3">{movieList}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Xu hướng phim */}
+
+        <div className="person-list">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="person">
+                  <p className="font-weight-bold-1  line-1" style={{ color: "white", paddingTop: 30 }}>
+                    <a href="#" className="hover-trending">NHỮNG DIỄN VIÊN</a> XU HƯỚNG TRONG TUẦN
+                  </p>
+                  <div className="knowwn">
+                    <div className="row">
+                      <div className="list-person list-sroll">
+                        {trendingPersons}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Phim có đánh giá cao nhất */}
+
+        <div className="top-rate">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <p className="font-weight-bold-1  line-1" style={{ color: "white" }}>
+                  <a href="#" className="hover-trending"> TOP PHIM ĐƯỢC ĐÁNH GIÁ CAO</a>
                 </p>
                 <div className="knowwn">
                   <div className="row">
                     <div className="list-person list-sroll">
-                      {trendingPersons}
+                      {topRatedList}
                     </div>
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Phim có đánh giá cao nhất */}
-
-      <div className="top-rate">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <p className="font-weight-bold-1  line-1" style={{ color: "white" }}>
-                <a href="#" className="hover-trending"> TOP PHIM ĐƯỢC ĐÁNH GIÁ CAO</a>
-              </p>
-              <div className="knowwn">
-                <div className="row">
-                  <div className="list-person list-sroll">
-                    {topRatedList}
+        <hr className="mt-5" style={{ border: "1px solid #5a606b" }}></hr>
+        <div className="footer-container">
+          <div className="container">
+            <div className="row">
+              <div className="bg"></div>
+              <div className="bg bg2"></div>
+              <div className="bg bg3"></div>
+              <div className="col-md-5 col-sm-6" style={{ color: "#5a606b" }}>
+                <h3>THÔNG TIN</h3>
+                <div className="email-title">
+                  Email :
+                  <a href="#">ngoctam2303001@gmail.com</a>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                  perspiciatis? Numquam, enim illo voluptatum neque facere aut sed ut
+                  dolore nihil? Nulla sit, recusandae ea tenetur rerum deserunt sequi
+                  earum?
+                </p>
+                <div className="button">
+                  <div className="icon">
+                    <i className="fab fa-facebook"></i>
                   </div>
+                  <span>Facebook</span>
+                </div>
+                <div className="button">
+                  <div className="icon">
+                    <i className="fab fa-instagram"></i>
+                  </div>
+                  <span>Instagram</span>
+                </div>
+                <div className="button">
+                  <div className="icon">
+
+                    <i className="fab fa-twitter"></i>
+                  </div>
+                  <span>Twitter</span>
+                </div>
+
+                <div className="button">
+                  <div className="icon">
+                    <i className="fab fa-youtube"></i>
+                  </div>
+                  <span>Youtube</span>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <hr className="mt-5" style={{ border: "1px solid #5a606b" }}></hr>
-      <div className="footer-container">
-
-        <div className="container">
-
-
-          <div className="row">
-            <div className="bg"></div>
-            <div className="bg bg2"></div>
-            <div className="bg bg3"></div>
-            <div className="col-md-5 col-sm-6" style={{ color: "#5a606b" }}>
-              <h3>THÔNG TIN</h3>
-              <div className="email-title">
-                Email :
-                <a href="#">ngoctam2303001@gmail.com</a>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-                perspiciatis? Numquam, enim illo voluptatum neque facere aut sed ut
-                dolore nihil? Nulla sit, recusandae ea tenetur rerum deserunt sequi
-                earum?
-              </p>
-              <div className="button">
-                <div className="icon">
-                  <i className="fab fa-facebook"></i>
-                </div>
-                <span>Facebook</span>
-              </div>
-              <div className="button">
-                <div className="icon">
-                  <i className="fab fa-instagram"></i>
-                </div>
-                <span>Instagram</span>
-              </div>
-              <div className="button">
-                <div className="icon">
-
-                  <i className="fab fa-twitter"></i>
-                </div>
-                <span>Twitter</span>
-              </div>
-
-              <div className="button">
-                <div className="icon">
-                  <i className="fab fa-youtube"></i>
-                </div>
-                <span>Youtube</span>
-              </div>
-            </div>
-            <div className="col-md-7 col-sm-6" style={{ color: "#5a606b" }}>
-              <div className="main-top">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="bg-blog">
-                        <div className="pic-ctn bore">
-                          {images}
+              <div className="col-md-7 col-sm-6" style={{ color: "#5a606b" }}>
+                <div className="main-top">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <div className="bg-blog">
+                          <div className="pic-ctn bore">
+                            {images}
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -332,13 +328,9 @@ export function Home() {
                 </div>
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
-
-
-
-    </div>
   );
 }
